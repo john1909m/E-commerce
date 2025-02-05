f624db9f2a2c766a5faf04f23d497bab5d7f9a47
john1909m <johnemil21@yahoo.com>
2025-02-05 18:18:37 +0200

    Update Home.js

--- Home.js
+++ Home.js
@@ -127,7 +127,7 @@ async function getDataLaptops(){
 function displayPhones(){
     let x=``
 
-    for(let i=0;i<6;i++){
+    for(let i=0;i<5;i++){
         x+=`<div class="product-card">
 
         <div class="product-card-img">
